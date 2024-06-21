9089d337bbe34f6e7436009c6e3d99a2bb622814
Kai Anter <kai@anter.dev>
2024-06-21 20:22:17 +0200

    fix(umami): follow redirects, add send to api path

--- umami_analytics/middleware.py
+++ umami_analytics/middleware.py
@@ -1,7 +1,7 @@
 import httpx
-from starlette.datastructures import Headers, MutableHeaders
+from starlette.datastructures import MutableHeaders
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.types import ASGIApp, Message, Receive, Scope, Send
+from starlette.types import ASGIApp
 from .umami_api import UmamiPayload
 from starlette.requests import Request
 from starlette.responses import Response
@@ -9,10 +9,11 @@ from starlette.background import BackgroundTask
 from dataclasses import asdict
 
 
-async def send_umami_payload(api_endpoint: str, payload: UmamiPayload, headers: MutableHeaders):
+async def send_umami_payload(api_endpoint: str, payload: UmamiPayload, headers: MutableHeaders, follow_redirects: bool):
     async with httpx.AsyncClient() as client:
         try:
-            await client.post(api_endpoint, json=asdict(payload), headers=headers)
+            resp = await client.post(api_endpoint, json=asdict(payload), headers=headers,
+                                     follow_redirects=follow_redirects)
         except Exception as e:
             # FIXME: How to do exception handling?
             print(f"Error sending umami payload: {e}")
@@ -25,14 +26,16 @@ class UmamiMiddleware(BaseHTTPMiddleware):
                  api_endpoint: str,
                  token: str,
                  website_id: str,
+                 follow_redirects: bool = True,
                  ) -> None:
         super().__init__(app)
         self.app = app
         if not api_endpoint.endswith('/'):
             api_endpoint += '/'
-        self.api_endpoint = api_endpoint
+        self.api_endpoint = api_endpoint + 'send'
         self.token = token
         self.website_id = website_id
+        self.follow_redirects = follow_redirects
 
     async def dispatch(self, request: Request, call_next) -> Response:
         response = await call_next(request)
@@ -53,6 +56,7 @@ class UmamiMiddleware(BaseHTTPMiddleware):
         umami_headers = MutableHeaders()
         umami_headers['X-Real-IP'] = request.client.host
         umami_headers['X-Forwarded-For'] = request.client.host
-        response.background = BackgroundTask(send_umami_payload, self.api_endpoint, payload, umami_headers)
+        response.background = BackgroundTask(send_umami_payload, self.api_endpoint, payload, umami_headers,
+                                             self.follow_redirects)
 
         return response
