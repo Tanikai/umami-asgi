0e2274b4598f556de7ed4db35ed5ca6ad4767720
Kai Anter <kai@anter.dev>
2024-06-21 20:23:36 +0200

    feat(example): added starlette example

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # umami-asgi-middleware
 Middleware for Umami Analytics in ASGI apps (FastAPI, Starlette, ...)
+
+
+
+## Examples
+
+FastAPI:
+
+```bash
+
+python -m uvicorn examples.fastapi_app:app --env-file examples/.env
+```
+
+Starlette:
+
+```bash
+python -m uvicorn examples.starlette_app:app --env-file examples/.env
+```
---
+++ examples/starlette_app.py
@@ -0,0 +1,29 @@
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.responses import JSONResponse
+from starlette.config import Config
+from starlette.routing import Route
+
+from umami_analytics.middleware import UmamiMiddleware
+
+config = Config()
+UMAMI_API_ENDPOINT = config('UMAMI_API_ENDPOINT')
+UMAMI_API_TOKEN = config('UMAMI_API_TOKEN')
+UMAMI_SITE_ID = config('UMAMI_SITE_ID')
+
+print(f"UMAMI_API_ENDPOINT: {UMAMI_API_ENDPOINT}")
+
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
+
+
+async def feed(request):
+    return JSONResponse({'posts': []})
+
+
+app = Starlette(debug=True, routes=[
+    Route('/', homepage),
+    Route('/feed', feed, methods=['GET', 'POST']),
+], middleware=[
+    Middleware(UmamiMiddleware, api_endpoint=UMAMI_API_ENDPOINT, token=UMAMI_API_TOKEN, website_id=UMAMI_SITE_ID),
+])
