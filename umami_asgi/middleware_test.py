6959b024d796e966cec0685f58b918f3a23cdb1b
Kai Anter <kai@anter.dev>
2024-06-22 18:02:52 +0200

    feat(umami): try to fix country tracking with headers and trusted proxies

--- umami_asgi/middleware_test.py
+++ umami_asgi/middleware_test.py
@@ -39,8 +39,8 @@ async def test_analytics_post(httpx_mock: HTTPXMock):
         # check whether middleware sent post request to mocked umami
         umami_request = httpx_mock.get_request()
         umami_payload = loads(umami_request.content.decode("utf-8"))["payload"]
-        assert umami_payload["name"] == "GET"
         assert umami_payload["url"] == "/"
+        assert umami_payload["ip"] == "testclient"
         # check whether middleware set headers correctly to track IP address of client, not asgi server
         assert umami_request.headers["X-Real-IP"] == "testclient"
         assert umami_request.headers["X-Forwarded-For"] == "testclient"
@@ -50,5 +50,60 @@ async def test_analytics_post(httpx_mock: HTTPXMock):
 
         umami_request = httpx_mock.get_requests()[1]
         umami_payload = loads(umami_request.content.decode("utf-8"))["payload"]
-        assert umami_payload["name"] == "POST"
         assert umami_payload["url"] == "/feed"
+
+
+@pytest.mark.asyncio
+async def test_trusted_proxies(httpx_mock: HTTPXMock):
+    httpx_mock.add_response()
+    app = Starlette(
+        routes=[
+            Route("/", endpoint=homepage),
+            Route("/feed", endpoint=feed, methods=["GET", "POST"]),
+        ],
+        middleware=[
+            Middleware(UmamiMiddleware, api_endpoint="https://localhost/api", website_id="123456", proxy_enabled=True,
+                       trusted_proxies=[])
+        ])
+
+    # fake forwarding headers
+    with TestClient(app, base_url="http://testserver", headers={
+        "X-Real-IP": "123.123.123.123",
+        "X-Forwarded-For": "111.111.111.111, 222.222.222.222",
+        "X-Forwarded-Host": "example.com",
+    }) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+
+        umami_request = httpx_mock.get_request()
+        h = umami_request.headers
+        assert h["X-Real-IP"] == "testclient"
+        assert h["X-Forwarded-For"] == "testclient"
+        assert h["X-Forwarded-Host"] == ""
+
+    app_trusted = Starlette(
+        routes=[
+            Route("/", endpoint=homepage),
+            Route("/feed", endpoint=feed, methods=["GET", "POST"]),
+        ],
+        middleware=[
+            Middleware(UmamiMiddleware, api_endpoint="https://localhost/api", website_id="123456", proxy_enabled=True,
+                       trusted_proxies=["testclient"])
+        ])
+
+    httpx_mock.reset(True)
+
+    with TestClient(app_trusted, base_url="http://testserver", headers={
+        "X-Real-IP": "123.123.123.123",
+        "X-Forwarded-For": "111.111.111.111, 222.222.222.222",
+        "X-Forwarded-Host": "example.com",
+    }) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+
+        umami_request = httpx_mock.get_request()
+        h = umami_request.headers
+        # see also https://stackoverflow.com/questions/72557636/difference-between-x-forwarded-for-and-x-real-ip-headers
+        assert h["X-Real-IP"] == "123.123.123.123"
+        assert h["X-Forwarded-For"] == "111.111.111.111, 222.222.222.222"
+        assert h["X-Forwarded-Host"] == "example.com"
